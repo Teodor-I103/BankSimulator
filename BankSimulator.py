ced25fef6649deed260411c3383ae0cc2868ca88
Teodor-I103 <teodor.iordan2131@gmail.com>
2025-05-16 11:51:41 +1200

    Completed function for verifying a valid user age.

--- BankSimulator.py
+++ BankSimulator.py
@@ -1,1 +1,21 @@
-print("This is the first commit")
+MAXIMUM_AGE = 18
+MINIMUM_AGE = 13
+
+def Valid_Age():
+    while True:
+        try:
+            user_age = int(input("Please enter your age: "))
+            if user_age > MAXIMUM_AGE:
+                print("You are too old to use this program.\nGoodbye!")
+                break
+            elif 0 < user_age < MINIMUM_AGE:
+                print("You are too young to use this program.\nGoodbye!")
+                break
+            elif user_age <= 0:
+                print("Enter an integer above 0")
+            else:
+                None
+        except ValueError:
+            print("Please enter an integer")
+
+Valid_Age()
